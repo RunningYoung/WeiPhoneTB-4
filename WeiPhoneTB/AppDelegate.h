01937fe3135df77889bf7ff0a7db5d6179c39ddf
马永群 <ibokan@ibk097mayongqun.local>
2013-06-18 10:48:09 +0800

    Initial Commit

---
+++ WeiPhoneTB/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WeiPhoneTB
+//
+//  Created by 马永群 on 13-6-18.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WeiPhoneTB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeiPhoneTB
+//
+//  Created by 马永群 on 13-6-18.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
